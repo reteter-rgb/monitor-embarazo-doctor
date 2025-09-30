be45ccad039b29ce8a8f2bc4d457cd23580eeefa
reteter-rgb <reteter@gmail.com>
2025-09-30 13:59:07 -0500

    Add files via upload

--- config/firebaseconfig.js
+++ config/firebaseconfig.js
@@ -1,11 +1,11 @@
-// Configuración de Firebase
+// Configuración REAL de Firebase - REEMPLAZA con tus datos
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO_ID",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBz-OtwR7FjXXxniTujLMfXl-hmhpbixJw",
+  authDomain: "monitor-embarazo-doctor.netlify.com",
+  projectId: "monitor-embarazo-doctor",
+  storageBucket: "monitor-embarazo-doctor.appspot.com",
+  messagingSenderId: "123456789000",
+  appId: "1:898197830613:android:e8bf61614b1d63dd4e9b5c"
 };
 
 // Inicializar Firebase
@@ -13,3 +13,9 @@ firebase.initializeApp(firebaseConfig);
 
 // Referencias a Firestore
 const db = firebase.firestore();
+
+// Habilitar persistencia offline (opcional pero recomendado)
+db.enablePersistence()
+  .catch((err) => {
+    console.log("Error en persistencia: ", err);
+  });
